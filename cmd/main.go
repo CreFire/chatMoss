1d8970ce470fd290f957c3eb3bfd32805a6d0bdb
MrLimit <1553448104@qq.com>
2023-02-20 01:18:37 +0800

    init

---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+
+	botToken := viper.GetString("telegramApiToken")
+	u, _ := url.Parse("http://127.0.0.1:10808")
+	client := &http.Client{
+		Transport: &http.Transport{
+			// 设置代理
+			Proxy: http.ProxyURL(u),
+		},
+	}
+	bot, err := tgbotapi.NewBotAPIWithClient(botToken, tgbotapi.APIEndpoint, client)
+	if err != nil {
+		panic(any(err))
+	}
+	bot.Debug = true
+}
+
+func init() {
+	viper.SetConfigFile("./config/config.yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		logrus.Errorf("config err:%v", err)
+		return
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Printf("Config file:%s Op:%s\n", e.Name, e.Op)
+	})
+	fmt.Printf("config: %v \n %v", viper.AllKeys(), viper.AllSettings())
+}
